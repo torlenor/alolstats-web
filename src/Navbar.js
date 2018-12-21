07460111dcfa814523924738eaa8dcea770145e5
Hans-Peter Schadler <hps@abyle.org>
2018-12-21 19:03:23 +0100

    get rid of warnings

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Nav, Navbar, NavItem, FormControl, Button, FormGroup  } from "react-bootstrap";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 
 class NavigationBar extends Component {
     render() {
