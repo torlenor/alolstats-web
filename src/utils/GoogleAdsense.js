2fd92718bde6d9dccbb35cb8e8852a048d25916b
Hans-Peter Schadler <hps@abyle.org>
2019-04-02 18:22:41 +0200

    Adsense test

---
+++ src/utils/GoogleAdsense.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Ad extends React.Component {
+  componentDidMount () {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }
+
+render () {
+    return (
+      <div className='ad'>
+        <ins className='adsbygoogle'
+          style={{ display: 'block' }}
+          data-ad-client='ca-pub-2362481387707104' 
+          enable_page_level_ads={true} />
+      </div>
+    );
+  }
+}
