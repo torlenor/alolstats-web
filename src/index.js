65080d7da9b820a8e8c464dd91f3f8c4bb456582
Hans-Peter Schadler <hps@abyle.org>
2019-08-13 21:24:40 +0200

    remove adsense

--- src/index.js
+++ src/index.js
@@ -8,14 +8,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { CookiesProvider } from 'react-cookie';
 
 import GA from './utils/GoogleAnalytics'
-import GAdsense from './utils/GoogleAdsense'
 
 ReactDOM.render(
     <Router>
         <CookiesProvider>
             { GA.init() && <GA.RouteTracker /> }
             <App />
-            { <GAdsense /> }
         </CookiesProvider>
     </Router>,
     document.getElementById("root")
